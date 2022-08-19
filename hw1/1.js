46a747262387a10dafd7a964400565cd6c636000
degtyarevevgeniy91 <degtyarev.evgeniy91@gmail.com>
2022-08-19 13:15:23 +0200

    hw4

--- hw1/1.js
+++ hw1/1.js
@@ -1,42 +1,42 @@
-let a;
-a = 'hello';
-console.log(a);
-let b;
-b = 'owu';
-console.log(b);
-let c;
-c = 'com';
-console.log(c);
-let d;
-d = 'ua';
-console.log(d);
-let e;
-e = 1;
-console.log(e);
-let f;
-f = 10;
-console.log(f);
-let g;
-g = -999;
-console.log(g);
-let h;
-h = 123;
-console.log(h);
-let i;
-i = 3.14;
-console.log(i);
-let j;
-j = 2.7;
-console.log(j);
-let k;
-k = 16;
-console.log(k);
-let l;
-l = true;
-console.log(l);
-let m;
-m = false;
-console.log(m);
+// let a;
+// a = 'hello';
+// console.log(a);
+// let b;
+// b = 'owu';
+// console.log(b);
+// let c;
+// c = 'com';
+// console.log(c);
+// let d;
+// d = 'ua';
+// console.log(d);
+// let e;
+// e = 1;
+// console.log(e);
+// let f;
+// f = 10;
+// console.log(f);
+// let g;
+// g = -999;
+// console.log(g);
+// let h;
+// h = 123;
+// console.log(h);
+// let i;
+// i = 3.14;
+// console.log(i);
+// let j;
+// j = 2.7;
+// console.log(j);
+// let k;
+// k = 16;
+// console.log(k);
+// let l;
+// l = true;
+// console.log(l);
+// let m;
+// m = false;
+// console.log(m);
 
 // alert(a);
 // alert(b);
@@ -51,28 +51,28 @@ console.log(m);
 // alert(l);
 // alert(m);
 
-document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g + ' ' +
-    h + ' ' + i + ' ' + k + ' ' + l + ' ' + m);
+// document.write(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g + ' ' +
+//     h + ' ' + i + ' ' + k + ' ' + l + ' ' + m);
+//
+// let firstName = 'Yvheny ';
+// let middleName = 'Andreevich ';
+// let lastName = 'Deghtyarev';
+// let person = firstName+middleName+lastName;
+// console.log(person)
+//
+//
+// let num1 = 100;
+// let num2 = '100';
+// let num3 = true;
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof num3);
 
-let firstName = 'Yvheny ';
-let middleName = 'Andreevich ';
-let lastName = 'Deghtyarev';
-let person = firstName+middleName+lastName;
-console.log(person)
 
 
-let num1 = 100;
-let num2 = '100';
-let num3 = true;
-console.log(typeof num1);
-console.log(typeof num2);
-console.log(typeof num3);
-
-
-
-let name =prompt('Введите Ваше Имя');
-let surname = prompt('Введите Ваше Отчество');
-let date = +prompt('Ваш возраст');
-console.log(name);
-console.log(surname);
-console.log(date);
+// let name =prompt('Введите Ваше Имя');
+// let surname = prompt('Введите Ваше Отчество');
+// let date = +prompt('Ваш возраст');
+// console.log(name);
+// console.log(surname);
+// console.log(date);
--- hw4/main.js
+++ hw4/main.js
@@ -105,35 +105,37 @@
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
 //
 
-let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
-];
-
-for (product of products) {
-    document.write(`<div class="product-title"><h2>${product.title} - ${product.price}</h2>
-<img src="${product.image}" alt="products">
-</div>`)
-
-}
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// for (product of products) {
+//     document.write(`<div class="product-card">
+//             <h3 class="product-title">${product.title} - ${product.price}</h3>
+//             <img class="product-image" src="${product.image}" alt="products">
+//             <button>BUY</button>
+//             </div>`)
+//
+// }
 
 
 
@@ -160,7 +162,186 @@ for (product of products) {
 //     {name: 'olya', age: 31, status: false},
 //     {name: 'max', age: 31, status: true}
 // ];
+//
+// for (user of users) {
+//     if (user.status) {
+//         console.log(user)
+//     }
+// }
+// console.log('-----------------------')
+// for (user of users) {
+//     if (!user.status) {
+//         console.log(user)
+//     }
+// }
+// console.log('-----------------------')
+// for (user of users) {
+//     if (user.age > 30) {
+//         console.log(user)
+//     }
+// }
+
 // за допомоги циклу вивести:
 //     - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
+
+
+// let books = [
+//     {
+//         name: 'Alice in Wanerland',
+//         pages: 256,
+//         author: ['Lewis Carrol'],
+//         genre: ['Childrens literature', 'fantasy', 'Literanure of absurd']
+//     },
+//     {
+//         name: 'Cinderella',
+//         pages: 134,
+//         author: ['Charles Perrault', 'Brothers Grimn'],
+//         genre: ['Fairy tale','Childrens literature']
+//     },
+//     {
+//         name: 'Harry Potter',
+//         pages: 452,
+//         author: ['Joan Rowling'],
+//         genre: ['fantasy']
+//     },
+// ]
+//     let max = books[0];
+// for (const book of books) {
+//     if (book.pages > max.pages) {
+//         max = book;
+//     }
+// }
+// console.log(max)
+
+
+// let maxGenre = books[0];
+// for (const book of books) {
+//     if (book.author > maxGenre.author) {
+//     maxGenre = book}
+// }
+// console.log(maxGenre)
+
+
+
+
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+
+// let array1 = [11, 25, 65, 15, 85];
+// let array2 = ['раз', 'два', 'три', 'четыри', 'пять'];
+// let array3 = ['Hello', 56, true];
+// const newArray = [array1,array2,array3];
+// console.log(newArray)
+
+//
+// -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+
+// let array = [];
+// array [0] = 'Hello';
+// array [1] = true;
+// array [2] = 25;
+// console.log(array[2])
+
+
+
+
+//
+//
+// - є масив [2,17,13,6,22,31,45,66,100,-18] :
+// 1. перебрати його циклом while
+//     2. перебрати його циклом for
+//     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+// 7. замінити кожне число кратне 3 на слово "okten"
+// 8. вивести масив в зворотньому порядку.
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+
+// let array = [2,17,13,6,22,31,45,66,100,-18];
+// let i = 0;
+// while (i < array.length) {
+//     console.log(array[i])
+//     i++;
+// }
+
+// let i = 0;
+// for (i = 0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+
+
+// let i = 0;
+// while (i < array.length) {
+//     if (i % 2 !==0){
+//         console.log(array[i])
+//     }
+//     i++;
+// }
+
+
+// let i = 0;
+// for (i = 0; i < array.length; i++) {
+//     if (i % 2 !==0) {
+//         console.log(array[i])
+//     }
+// }
+// let array = [2,17,13,6,22,31,45,66,100,-18];
+// let i = 0;
+// while (i < array.length) {
+//     if (i % 3 ===0) {
+//         array[i] = 'okten';
+//     }
+//     console.log(array[i])
+//     i++
+// }
+
+
+
+
+
+//
+// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+//
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+// - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+// - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
